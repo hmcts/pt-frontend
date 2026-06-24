7c8e15d8caa0d5016970cffdbb17a7a362b45f09
Tom Elliott <tomxelliott@hotmail.co.uk>
2026-06-24 14:57:13 +0100

    add route to throw error to test error page

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -4,4 +4,8 @@ export default function (app: Application): void {
   app.get('/', (req, res) => {
     res.render('home');
   });
+
+  app.get('/test-error', (_req, _res) => {
+    throw new Error();
+  });
 }
