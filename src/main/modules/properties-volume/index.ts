2b5d2fb1db0207fc503423188566e060936cf4d3
Tom Elliott <tomxelliott@hotmail.co.uk>
2026-06-30 17:23:49 +0100

    fix: correct aliases in properties volume

--- src/main/modules/properties-volume/index.ts
+++ src/main/modules/properties-volume/index.ts
@@ -7,11 +7,13 @@ export class PropertiesVolume {
     if (environment !== 'development') {
       propertiesVolume.addTo(config);
 
-      await this.setSecret('secrets.pt.AppInsightsInstrumentationKey', 'appInsights.instrumentationKey');
-      await this.setSecret('secrets.pt.idam-system-user-name', 'idam.systemUsername');
-      await this.setSecret('secrets.pt.idam-system-user-password', 'idam.systemPassword');
-      await this.setSecret('secrets.pt.pt-frontend-idam-secret', 'idam.clientSecret');
-      await this.setSecret('secrets.pt.pt-frontend-s2s-secret', 'authProvider.secret');
+      await this.setSecret('secrets.pt-kv.app-insights-connection-string', 'appInsights.instrumentationKey');
+      await this.setSecret('secrets.pt-kv.redis-connection-string', 'idam.systemUsername');
+      await this.setSecret('secrets.pt-kv.pt-session-secret', 'idam.systemUsername');
+      await this.setSecret('secrets.pt-kv.idam-system-user-name', 'idam.systemUsername');
+      await this.setSecret('secrets.pt-kv.idam-system-user-password', 'idam.systemPassword');
+      await this.setSecret('secrets.pt-kv.pt-frontend-idam-secret', 'idam.clientSecret');
+      await this.setSecret('secrets.pt-kv.pt-frontend-s2s-secret', 'authProvider.secret');
     }
   }
 
