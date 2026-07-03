997eda8cd15db3c788297e21b6777928bd62c9c8
Tom Elliott <tomxelliott@hotmail.co.uk>
2026-07-03 15:35:38 +0100

    fix: ensure the async properties volume function is awaited

--- src/main/modules/properties-volume/index.ts
+++ src/main/modules/properties-volume/index.ts
@@ -12,6 +12,7 @@ export class PropertiesVolume {
       await this.setSecret('secrets.pt.idam-system-user-password', 'idam.systemPassword');
       await this.setSecret('secrets.pt.pt-frontend-idam-secret', 'idam.clientSecret');
       await this.setSecret('secrets.pt.pt-frontend-s2s-secret', 'authProvider.secret');
+      await this.setSecret('secrets.pt.redis-connection-string', 'session.redis-connection-string');
     }
   }
 
