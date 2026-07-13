4c349adf3df2fb867ad7e50e37d1a1ab05154fbf
Tom Elliott <tomxelliott@hotmail.co.uk>
2026-07-13 12:20:27 +0100

    feat: add user check to root route

--- src/test/unit/routes/myApplications.test.ts
+++ src/test/unit/routes/myApplications.test.ts
@@ -3,7 +3,38 @@ import { Application, Request, Response } from 'express';
 import myApplications from '@routes/myApplications';
 
 describe('my applications route', () => {
-  it('should render the myApplications view on GET /', () => {
+  it('should render the myApplications view when logged in on GET /', () => {
+    let handler: (req: Request, res: Response) => void = () => {};
+
+    const app = {
+      get: jest.fn((path: string, cb: (req: Request, res: Response) => void) => {
+        if (path === '/') {
+          handler = cb;
+        }
+      }),
+    } as unknown as Application;
+
+    myApplications(app);
+
+    expect(app.get).toHaveBeenCalledWith('/', expect.any(Function));
+
+    const req = {
+      session: {
+        user: {
+          email: 'test@email.com',
+        },
+      },
+    } as unknown as Request;
+    const res = {
+      render: jest.fn(),
+    } as unknown as Response;
+
+    handler(req, res);
+
+    expect(res.render).toHaveBeenCalledWith('myApplications');
+  });
+
+  it('should redirect to login when not logged in on GET /', () => {
     let handler: (req: Request, res: Response) => void = () => {};
 
     const app = {
@@ -20,11 +51,11 @@ describe('my applications route', () => {
 
     const req = {} as Request;
     const res = {
-      render: jest.fn(),
+      redirect: jest.fn(),
     } as unknown as Response;
 
     handler(req, res);
 
-    expect(res.render).toHaveBeenCalledWith('myApplications');
+    expect(res.redirect).toHaveBeenCalledWith('/login');
   });
 });
