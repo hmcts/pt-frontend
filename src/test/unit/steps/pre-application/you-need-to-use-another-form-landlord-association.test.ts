ce144a036b8890d933f32aed8a5e6e5934ba35fe
Tom Elliott <tomxelliott@hotmail.co.uk>
2026-07-17 17:54:10 +0100

    feat: update tests

--- src/test/unit/steps/pre-application/you-need-to-use-another-form-landlord-association.test.ts
+++ src/test/unit/steps/pre-application/you-need-to-use-another-form-landlord-association.test.ts
@@ -2,32 +2,35 @@ import { Request } from 'express';
 
 import { flowConfig } from '../../../../main/steps/pre-application/flow.config';
 
+import { createFormStep } from '@modules/steps';
 import { getPreviousStep } from '@modules/steps/flow';
 
-jest.mock('../../../../main/modules/steps/i18n', () => ({
-  loadStepNamespace: jest.fn(),
-  getStepTranslations: jest.fn(() => ({})),
-  getTranslationFunction: jest.fn(() => (key: string) => key),
+import './../../../../main/steps/pre-application/you-need-to-use-another-form-landlord-association/index';
+
+jest.mock('@modules/steps', () => ({
+  createFormStep: jest.fn(),
 }));
 
-jest.mock('../../../../main/modules/i18n', () => ({
-  getRequestLanguage: jest.fn(() => 'en'),
-  getCommonTranslations: jest.fn(() => ({})),
-}));
+describe('you-need-to-use-another-form-postcode step', () => {
+  const mockCreateFormStep = createFormStep as jest.Mock;
 
-jest.mock('../../../../main/modules/steps/formBuilder/helpers', () => {
-  const actual = jest.requireActual('../../../../main/modules/steps/formBuilder/helpers');
-  return {
-    ...actual,
-    validateForm: jest.fn(),
-  };
-});
+  const capturedConfig = mockCreateFormStep.mock.calls[0][0];
 
-describe('back navigation from you-need-to-use-another-form', () => {
-  it('uses landlord-is-a-housing-association as previous step', async () => {
-    const req = {} as Request;
-    await expect(
-      getPreviousStep(req, 'you-need-to-use-another-form-landlord-association', flowConfig, {})
-    ).resolves.toBe('landlord-is-a-housing-association');
+  it('passes the expected static config to createFormStep', () => {
+    expect(mockCreateFormStep).toHaveBeenCalledTimes(1);
+    expect(capturedConfig.stepName).toBe('you-need-to-use-another-form-landlord-association');
+    expect(capturedConfig.journeyFolder).toBe('preApplication');
+    expect(capturedConfig.showCancelButton).toBe(false);
+    expect(capturedConfig.fields).toEqual([]);
+    expect(capturedConfig.translationKeys).toEqual({ pageTitle: 'questionTitle' });
+  });
+
+  describe('back navigation from you-need-to-use-another-form', () => {
+    it('uses landlord-is-a-housing-association as previous step', async () => {
+      const req = {} as Request;
+      await expect(
+        getPreviousStep(req, 'you-need-to-use-another-form-landlord-association', flowConfig, {})
+      ).resolves.toBe('landlord-is-a-housing-association');
+    });
   });
 });
