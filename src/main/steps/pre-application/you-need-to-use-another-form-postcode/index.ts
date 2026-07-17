30f69e9e3ee5431de3c27f719003afcc0fe8bce6
Tom Elliott <tomxelliott@hotmail.co.uk>
2026-07-17 17:15:30 +0100

    feat: refactor you need to use steps to utilise createFormStep

--- src/main/steps/pre-application/you-need-to-use-another-form-postcode/index.ts
+++ src/main/steps/pre-application/you-need-to-use-another-form-postcode/index.ts
@@ -1,27 +1,26 @@
-import type { Request } from 'express';
+import { flowConfig } from '../flow.config';
 
-import { getFlowConfigForJourney } from '../../index';
-import { PRE_APPLICATION_ROUTE, flowConfig } from '../flow.config';
-
-import { createGetController, createStepNavigation } from '@modules/steps';
+import { createFormStep } from '@modules/steps';
 import type { StepDefinition } from '@modules/steps/stepFormData.interface';
 
 const journeyName = 'preApplication';
 const stepName = 'you-need-to-use-another-form-postcode';
-const templatePath = 'pre-application/you-need-to-use-another-form-postcode/youNeedToUseAnotherFormPostcode.njk';
-const stepNavigation = createStepNavigation(() => getFlowConfigForJourney(journeyName) || flowConfig);
 
-export const step: StepDefinition = {
-  url: `${PRE_APPLICATION_ROUTE}/you-need-to-use-another-form-postcode`,
-  name: stepName,
-  view: templatePath,
+export const step: StepDefinition = createFormStep({
+  stepName,
+  journeyFolder: journeyName,
   stepDir: __dirname,
-  getController: () => {
-    return createGetController(templatePath, stepName, stepNavigation, (req: Request) => {
-      const postcode = req.session.formData?.['address-of-property']?.addressPostcode;
-      return {
-        postcode,
-      };
-    });
+  flowConfig,
+  customTemplate: `${__dirname}/youNeedToUseAnotherFormPostcode.njk`,
+  showCancelButton: false,
+  translationKeys: {
+    pageTitle: 'questionTitle',
   },
-};
+  fields: [],
+  extendGetContent: req => {
+    const postcode = req.session.formData?.['address-of-property']?.addressPostcode;
+    return {
+      postcode,
+    };
+  },
+});
--- src/main/steps/pre-application/you-need-to-use-another-form/index.ts
+++ src/main/steps/pre-application/you-need-to-use-another-form/index.ts
@@ -1,20 +1,20 @@
-import { getFlowConfigForJourney } from '../../index';
-import { PRE_APPLICATION_ROUTE, flowConfig } from '../flow.config';
+import { flowConfig } from '../flow.config';
 
-import { createGetController, createStepNavigation } from '@modules/steps';
+import { createFormStep } from '@modules/steps';
 import type { StepDefinition } from '@modules/steps/stepFormData.interface';
 
 const journeyName = 'preApplication';
 const stepName = 'you-need-to-use-another-form';
-const templatePath = 'pre-application/you-need-to-use-another-form/youNeedToUseAnotherForm.njk';
-const stepNavigation = createStepNavigation(() => getFlowConfigForJourney(journeyName) || flowConfig);
 
-export const step: StepDefinition = {
-  url: `${PRE_APPLICATION_ROUTE}/you-need-to-use-another-form`,
-  name: stepName,
-  view: templatePath,
+export const step: StepDefinition = createFormStep({
+  stepName,
+  journeyFolder: journeyName,
   stepDir: __dirname,
-  getController: () => {
-    return createGetController(templatePath, stepName, stepNavigation);
+  flowConfig,
+  customTemplate: `${__dirname}/youNeedToUseAnotherForm.njk`,
+  showCancelButton: false,
+  translationKeys: {
+    pageTitle: 'questionTitle',
   },
-};
+  fields: [],
+});
