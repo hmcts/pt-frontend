d8cfa9d47a4e01196d49b9abfaa970c33efce2ba
Tom Elliott <tomxelliott@hotmail.co.uk>
2026-07-23 14:06:42 +0100

    feat: create screens for new application before case creation in ccd

--- src/main/case/case.ts
+++ src/main/case/case.ts
@@ -2,6 +2,8 @@ export interface Case {
   //TODO: build this out once data model added to pt-api
   firstName: string;
   lastName: string;
+  applicationType: string;
+  tenancyType: string;
 }
 
 export interface CaseWithId extends Case {
---
+++ src/main/steps/new-application/tenancy-type/index.ts
@@ -0,0 +1,59 @@
+import { Request } from 'express';
+
+import { getCaseApi } from '../../../case/ccdApiClient';
+import { flowConfig } from '../flow.config';
+
+import { createFormStep, getFormData } from '@modules/steps';
+import type { StepDefinition } from '@modules/steps/stepFormData.interface';
+
+const journeyName = 'newApplication';
+const stepName = 'tenancy-type';
+
+export const step: StepDefinition = createFormStep({
+  stepName,
+  journeyFolder: journeyName,
+  stepDir: __dirname,
+  flowConfig,
+  customTemplate: `${__dirname}/tenancyType.njk`,
+  showCancelButton: false,
+  translationKeys: {
+    pageTitle: 'questionTitle',
+  },
+  fields: [
+    {
+      name: 'tenancyType',
+      type: 'radio',
+      required: true,
+      isPageHeading: true,
+      legendClasses: 'govuk-fieldset__legend--l',
+      translationKey: { label: 'questionTitle' },
+      errorMessage: 'errors.tenancyType.required',
+      options: [
+        {
+          value: 'assuredPeriodicTenancy',
+          translationKey: 'options.assuredPeriodicTenancy.label',
+          hint: 'options.assuredPeriodicTenancy.hint',
+        },
+        { value: 'agriculturalOccupancy', translationKey: 'options.agriculturalOccupancy.label' },
+      ],
+    },
+  ],
+  beforeRedirect: async (req: Request) => {
+    const ccdCaseApi = getCaseApi(req.session.user);
+
+    const applicationType: string = getFormData(req, 'application-type').applicationType as string;
+    const tenancyType: string = getFormData(req, 'tenancy-type').tenancyType as string;
+
+    const ccdCase = await ccdCaseApi.createCase({
+      applicationType,
+      tenancyType,
+    });
+
+    //  TODO: resolve    message: 'Cannot find event citizen-create-application for case type PT',
+    const caseReference = ccdCase.id;
+    // const caseReference = '1234123412341234';
+    const redirectPath = `/case/${caseReference}/application/task-list`;
+
+    return req.res!.redirect(303, redirectPath);
+  },
+});
