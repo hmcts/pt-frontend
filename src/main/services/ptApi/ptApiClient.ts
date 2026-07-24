315f98ac59f35142349030e0194291c84b2bb769
Tom Elliott <tomxelliott@hotmail.co.uk>
2026-07-24 16:04:50 +0100

    feat: rework ccd data objects used

--- src/main/services/ptApi/ptApiClient.ts
+++ src/main/services/ptApi/ptApiClient.ts
@@ -1,10 +1,9 @@
 import axios, { AxiosInstance } from 'axios';
 import config from 'config';
 
-import { getServiceAuthToken } from '../auth/service/get-service-auth-token';
-import { UserDetails } from '../auth/user/oidc';
-
-import { CaseWithId } from './case';
+import { getServiceAuthToken } from '../../auth/service/get-service-auth-token';
+import { UserDetails } from '../../auth/user/oidc';
+import { ApplicationData } from '../ccdCase.interface';
 
 import { Logger } from '@modules/logger';
 
@@ -13,9 +12,9 @@ const logger = Logger.getLogger('service-auth-token');
 export class PtApiClient {
   constructor(private readonly client: AxiosInstance) {}
 
-  async getAllCasesByUser(): Promise<CaseWithId> {
+  async getAllCasesByUser(): Promise<ApplicationData> {
     try {
-      const response = await this.client.get<CaseWithId>('/applications');
+      const response = await this.client.get<ApplicationData>('/applications');
       return response.data;
     } catch (err) {
       logger.error(err);
@@ -23,9 +22,9 @@ export class PtApiClient {
     }
   }
 
-  async getCaseByCaseReference(caseReference: string): Promise<CaseWithId[]> {
+  async getCaseByCaseReference(caseReference: string): Promise<ApplicationData[]> {
     try {
-      const response = await this.client.get<CaseWithId[]>(`/applications/${caseReference}`);
+      const response = await this.client.get<ApplicationData[]>(`/applications/${caseReference}`);
       return response.data;
     } catch (err) {
       logger.error(err);
