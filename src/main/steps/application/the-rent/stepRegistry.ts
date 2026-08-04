b9994013569234c6973ecc3ea682a18ad37113f3
OscarAR <oscar.a-r@hotmail.com>
2026-08-04 16:54:51 +0100

    chore: add rent frequency step stub

--- src/main/steps/application/the-rent/stepRegistry.ts
+++ src/main/steps/application/the-rent/stepRegistry.ts
@@ -1,9 +1,11 @@
 import { step as currentRentAndOtherCosts } from './the-current-rent-and-other-costs/current-rent-and-other-costs';
+import { step as rentFrequency } from './the-current-rent-and-other-costs/rent-frequency';
 import { step as proposedMarketRent } from './what-you-think-market-rent-should-be/proposed-market-rent';
 
 import type { StepDefinition } from '@modules/steps/stepFormData.interface';
 
 export const theRentStepRegistry = {
   'current-rent-and-other-costs': currentRentAndOtherCosts,
+  'rent-frequency': rentFrequency,
   'proposed-market-rent': proposedMarketRent,
 } satisfies Record<string, StepDefinition>;
