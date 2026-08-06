5e67f7fa9d5f91e17f5832ebe7a7dd6187f41082
OscarAR <oscar.a-r@hotmail.com>
2026-08-06 15:47:28 +0100

    chore: add rent includes council tax step stub

--- src/main/steps/application/the-rent/stepRegistry.ts
+++ src/main/steps/application/the-rent/stepRegistry.ts
@@ -1,4 +1,5 @@
 import { step as currentRentAndOtherCosts } from './the-current-rent-and-other-costs/current-rent-and-other-costs';
+import { step as rentIncludesCouncilTax } from './the-current-rent-and-other-costs/rent-includes-council-tax';
 import { step as rentPaymentFrequency } from './the-current-rent-and-other-costs/rent-payment-frequency';
 import { step as tribunalPreviouslyDeterminedRent } from './the-current-rent-and-other-costs/tribunal-previously-determined-rent';
 import { step as proposedMarketRent } from './what-you-think-market-rent-should-be/proposed-market-rent';
@@ -9,5 +10,6 @@ export const theRentStepRegistry = {
   'current-rent-and-other-costs': currentRentAndOtherCosts,
   'tribunal-previously-determined-rent': tribunalPreviouslyDeterminedRent,
   'rent-payment-frequency': rentPaymentFrequency,
+  'rent-includes-council-tax': rentIncludesCouncilTax,
   'proposed-market-rent': proposedMarketRent,
 } satisfies Record<string, StepDefinition>;
