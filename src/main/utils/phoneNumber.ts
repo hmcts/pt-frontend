ceea75547a225278ece8d94e21b54bbc0db9525c
Tom Elliott <tomxelliott@hotmail.co.uk>
2026-08-11 10:46:43 +0100

    fix: update validation for mobile phones vs landline numbers

--- src/main/utils/phoneNumber.ts
+++ src/main/utils/phoneNumber.ts
@@ -1,7 +1,12 @@
-export function isValidPhoneNumber(phoneNumber: string): boolean {
+// validates whether a number is a valid landline or mobile number
+export const VALID_PHONE_NUMBER_REGEX = /^(\+[1-9]\d{1,14}|0\d{10})$/;
+
+// validates whether a number is a valid mobile phone number only
+export const VALID_MOBILE_NUMBER_REGEX = /^(\+447\d{9}|07\d{9})$/;
+
+export function isValidPhoneNumber(phoneNumber: string, regex: RegExp): boolean {
   // first strip the phone number of any whitespace/formatting, keep digits and leading +
   const sanitisedPhoneNumber = phoneNumber.replace(/[^\d+]/g, '');
-  // validates whether sanitisedPhoneNumber is a valid UK number (mobile or landline, starting 0)
-  // or an international number (starting +)
-  return /^(\+[1-9]\d{1,14}|0\d{10})$/.test(sanitisedPhoneNumber);
+  // validates whether sanitisedPhoneNumber against provided regex
+  return regex.test(sanitisedPhoneNumber);
 }
