eaed731b254a1bbbfcaf0e5183791475be36938b
OscarAR <oscar.a-r@hotmail.com>
2026-08-14 15:57:38 +0100

    feat: add rent inclusive of utility charges field to case data

--- src/main/services/ccdCase.interface.ts
+++ src/main/services/ccdCase.interface.ts
@@ -145,4 +145,7 @@ export interface RentDetails {
   councilTaxCostMonthly?: string;
   councilTaxCostYearly?: string;
   councilTaxFrequencyAndCostDetails?: string;
+
+  // utilities
+  rentInclusiveOfUtilityCharges?: YesNoValue;
 }
