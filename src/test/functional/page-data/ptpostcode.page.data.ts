de92302e0ef4789aa8a6aa699fcc7b11d82b60c2
Lakshmi Devarapalli <lakshmi.devarapalli@hmcts.net>
2026-08-24 13:25:47 +0100

    Add HDPD-564 postcode validation UI test

---
+++ src/test/functional/page-data/ptpostcode.page.data.ts
@@ -0,0 +1,9 @@
+export const postcodeData = {
+  rolloutPostcode: 'SW1A 1AA',
+  nonRolloutPostcode: 'LS1 1AA',
+  nonEnglishPostcode: 'CF10 1EP',
+  invalidPostcode: 'ABC123',
+  emptyPostcode: '',
+  errorMessage: 'Enter a valid postcode',
+  postalAddressCode: '#addressPostcode',
+};
