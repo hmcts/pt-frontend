82bb15e8712bee8d97d6432cd11abb96a5837a6d
Lakshmi Devarapalli <lakshmi.devarapalli@hmcts.net>
2026-08-24 13:26:09 +0100

    Add HDPD-636 landlord housing association UI test

---
+++ src/test/functional/page-data/ptLandlordHousingAssociation.page.data.ts
@@ -0,0 +1,10 @@
+export const landlordHousingAssociation = {
+  pageHeading: 'Is your landlord a housing association?',
+  yesOption: 'Yes',
+  noOption: 'No',
+  continueButton: 'Continue',
+  validationErrorMessage: 'Select whether your landlord is a housing association',
+  anotherFormPageHeading: 'You need to use another form to apply',
+  landlordapplicationTypeHeading: 'Application type',
+  landingHousingAssociationUrl: '/pre-application/landlord-is-a-housing-association',
+};
