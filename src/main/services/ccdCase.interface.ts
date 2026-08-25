ec336e7652db233a3ddd03e527a34055f13bafba
Alex <alex.buzea@hmcts.net>
2026-08-25 12:14:17 +0100

    adding inspection page and placeholder for hearing page

--- src/main/services/ccdCase.interface.ts
+++ src/main/services/ccdCase.interface.ts
@@ -26,7 +26,8 @@ export interface CcdCollectionItem<T> {
 export type CaseData = CcdCaseData;
 
 /** Case data payload from CCD (START callback case_data or CcdCase.data). */
-export interface CcdCaseData extends ContactPreferences, LandlordDetails, PropertyDetails, LettingAgentDetails {
+export interface CcdCaseData
+  extends ContactPreferences, LandlordDetails, PropertyDetails, LettingAgentDetails, InspectionAndHearing {
   //TODO: build this out once data model added to pt-api
   applicantFirstName?: string;
   applicantLastName?: string;
@@ -77,7 +78,8 @@ export interface StartCallbackData {
 
 //
 /** Case data payload returned from PT API get case(s) calls */
-export interface ApplicationData extends ContactPreferences, LandlordDetails, PropertyDetails, LettingAgentDetails {
+export interface ApplicationData
+  extends ContactPreferences, LandlordDetails, PropertyDetails, LettingAgentDetails, InspectionAndHearing {
   caseReference: bigint;
   createdDate: string;
   submittedOn?: string;
@@ -124,4 +126,9 @@ export interface PropertyDetails {
   servicesProvided?: string | boolean;
   servicesProvidedDetails?: string;
   hasRepairsAndImprovements?: string | boolean;
+}
+
+export interface InspectionAndHearing {
+  agreeToDecisionWithoutInspection?: string | boolean;
+  noDecisionWithoutInspectionReason?: string;
 }
