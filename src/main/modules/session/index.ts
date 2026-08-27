25ef7a3e2a2d72ee63f87000bff328d2682be3b4
Tom Elliott <tomxelliott@hotmail.co.uk>
2026-08-27 17:30:23 +0100

    fix: stop logging redis key when connection error occurs

--- src/main/modules/session/index.ts
+++ src/main/modules/session/index.ts
@@ -19,8 +19,7 @@ export class Session {
     });
 
     redis.on('error', (err: typeof Error) => {
-      this.logger.error(`REDIS CONNECTION ERROR: ${redisConnectionString}`);
-      this.logger.error('REDIS ERROR:', err);
+      this.logger.error(`Redis connection error (${new URL(redisConnectionString).host})`, err);
     });
 
     redis.on('ready', () => {
