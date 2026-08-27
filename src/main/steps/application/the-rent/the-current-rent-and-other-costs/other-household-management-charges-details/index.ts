bfd8cf8884ba9a91400ea08e8292ce204685c8ea
OscarAR <oscar.a-r@hotmail.com>
2026-08-27 15:36:49 +0100

    feat: add other charges optional page

--- src/main/steps/application/the-rent/the-current-rent-and-other-costs/other-household-management-charges-details/index.ts
+++ src/main/steps/application/the-rent/the-current-rent-and-other-costs/other-household-management-charges-details/index.ts
@@ -1,10 +1,14 @@
+import { textAreaIsValidLength } from '../../../../utils/fieldValidators';
 import { flowConfig } from '../../../flow.config';
 
 import { createFormStep } from '@modules/steps';
 import type { StepDefinition } from '@modules/steps/stepFormData.interface';
+import { CcdCaseData } from '@services/ccdCase.interface';
 
 const journeyName = 'application';
 const stepName = 'other-household-management-charges-details';
+
+const fieldName = 'otherHouseholdManagementChargesDetails';
 
 export const step: StepDefinition = createFormStep({
   stepName,
@@ -13,9 +17,30 @@ export const step: StepDefinition = createFormStep({
   flowConfig,
   customTemplate: `${__dirname}/otherHouseholdManagementChargesDetails.njk`,
   showCancelButton: false,
-  isAnswered: () => false,
-  translationKeys: {
-    heading: 'heading',
-  },
-  fields: [],
+  isAnswered: req => isAnswered(req.session.ccdCase),
+  translationKeys: { pageTitle: 'pageTitle' },
+  fields: [
+    {
+      name: fieldName,
+      type: 'textarea',
+      required: false,
+      isPageHeading: false,
+      labelClasses: 'govuk-label--m',
+      maxLength: 500,
+      translationKey: { label: 'questionTitle' },
+      validator: (value: unknown): boolean | string => {
+        if (!textAreaIsValidLength(value as string)) {
+          return `errors.${fieldName}.invalid`;
+        }
+        return true;
+      },
+    },
+  ],
 });
+
+function isAnswered(ccdCase: CcdCaseData): boolean {
+  if (ccdCase.otherHouseholdManagementChargesDetails === undefined) {
+    return false;
+  }
+  return textAreaIsValidLength(ccdCase.otherHouseholdManagementChargesDetails);
+}
