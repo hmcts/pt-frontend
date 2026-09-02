272392cc24e2167737aceee6e7b72008e4c9c027
Alex Buzea <60071473+AlexBuzea@users.noreply.github.com>
2026-09-02 11:34:42 +0100

    feat/HDPD-1050-hearing-page (#325)
    * adding inspection page and placeholder for hearing page
    
    * adding hearing page and placeholder for cya page

--- src/main/steps/application/inspection-and-hearing/stepRegistry.ts
+++ src/main/steps/application/inspection-and-hearing/stepRegistry.ts
@@ -1,3 +1,4 @@
+import { step as checkYourAnswersInspectionAndHearing } from './check-your-answers-inspection-and-hearing';
 import { step as hearing } from './hearing';
 import { step as propertyInspection } from './property-inspection';
 
@@ -6,4 +7,5 @@ import type { StepDefinition } from '@modules/steps/stepFormData.interface';
 export const inspectionAndHearingStepRegistry = {
   'property-inspection': propertyInspection,
   hearing,
+  'check-your-answers-inspection-and-hearing': checkYourAnswersInspectionAndHearing,
 } satisfies Record<string, StepDefinition>;
