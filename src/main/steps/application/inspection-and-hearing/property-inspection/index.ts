4cd5968020409aea9c1488896333447cd6f92b60
Alex Buzea <60071473+AlexBuzea@users.noreply.github.com>
2026-09-02 11:07:45 +0100

    adding inspection page and placeholder for hearing page (#311)

--- src/main/steps/application/inspection-and-hearing/property-inspection/index.ts
+++ src/main/steps/application/inspection-and-hearing/property-inspection/index.ts
@@ -1,7 +1,9 @@
+import { textAreaIsValidLength } from '../../../utils/fieldValidators';
 import { flowConfig } from '../../flow.config';
 
 import { createFormStep } from '@modules/steps';
 import type { StepDefinition } from '@modules/steps/stepFormData.interface';
+import { CcdCaseData } from '@services/ccdCase.interface';
 
 const journeyName = 'application';
 const stepName = 'property-inspection';
@@ -13,10 +15,52 @@ export const step: StepDefinition = createFormStep({
   flowConfig,
   customTemplate: `${__dirname}/propertyInspection.njk`,
   showCancelButton: false,
-  isAnswered: () => false,
-  translationKeys: {
-    pageTitle: 'pageTitle',
-    heading: 'heading',
-  },
-  fields: [],
+  isAnswered: req => isAnswered(req.session.ccdCase),
+  fields: [
+    {
+      name: 'agreeToDecisionWithoutInspection',
+      type: 'radio',
+      required: true,
+      isPageHeading: false,
+      legendClasses: 'govuk-fieldset__legend--m',
+      translationKey: { label: 'questionTitle' },
+      errorMessage: 'errors.agreeToDecisionWithoutInspection.required',
+      options: [
+        { value: 'yes', translationKey: 'common:yes' },
+        {
+          value: 'no',
+          translationKey: 'common:no',
+          subFields: {
+            noDecisionWithoutInspectionReason: {
+              name: 'noDecisionWithoutInspectionReason',
+              type: 'textarea',
+              maxLength: 500,
+              required: true,
+              errorMessage: 'errors.noDecisionWithoutInspectionReason.required',
+              translationKey: {
+                label: 'options.noDecisionWithoutInspectionReason.label',
+                hint: 'options.noDecisionWithoutInspectionReason.hint',
+              },
+              validator: (value): boolean | string => {
+                if (value && String(value).length > 500) {
+                  return 'errors.noDecisionWithoutInspectionReason.invalid';
+                }
+                return true;
+              },
+            },
+          },
+        },
+      ],
+    },
+  ],
 });
+
+function isAnswered(ccdCase: CcdCaseData): boolean {
+  if (ccdCase.agreeToDecisionWithoutInspection === 'no') {
+    return Boolean(
+      (ccdCase.noDecisionWithoutInspectionReason as string) &&
+      textAreaIsValidLength(ccdCase.noDecisionWithoutInspectionReason as string)
+    );
+  }
+  return ccdCase.agreeToDecisionWithoutInspection === 'yes';
+}
--- src/main/steps/application/inspection-and-hearing/stepRegistry.ts
+++ src/main/steps/application/inspection-and-hearing/stepRegistry.ts
@@ -1,7 +1,9 @@
+import { step as hearing } from './hearing';
 import { step as propertyInspection } from './property-inspection';
 
 import type { StepDefinition } from '@modules/steps/stepFormData.interface';
 
 export const inspectionAndHearingStepRegistry = {
   'property-inspection': propertyInspection,
+  hearing,
 } satisfies Record<string, StepDefinition>;
