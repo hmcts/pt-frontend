28b70ae580b57342193c9f0bbec252ed342852c8
Tom Elliott <tomxelliott@hotmail.co.uk>
2026-09-03 14:00:37 +0100

    feat: fix issues after merging master in feature branch

--- src/main/steps/application/inspection-and-hearing/property-inspection/index.ts
+++ src/main/steps/application/inspection-and-hearing/property-inspection/index.ts
@@ -3,7 +3,7 @@ import { flowConfig } from '../../flow.config';
 
 import { createFormStep } from '@modules/steps';
 import type { StepDefinition } from '@modules/steps/stepFormData.interface';
-import { CcdCaseData } from '@services/ccdCase.interface';
+import { PTCaseData } from '@services/ccdCase.interface';
 
 const journeyName = 'application';
 const stepName = 'property-inspection';
@@ -55,12 +55,12 @@ export const step: StepDefinition = createFormStep({
   ],
 });
 
-function isAnswered(ccdCase: CcdCaseData): boolean {
-  if (ccdCase.agreeToDecisionWithoutInspection === 'no') {
+function isAnswered(ccdCase: PTCaseData | undefined): boolean {
+  if (ccdCase?.agreeToDecisionWithoutInspection === 'no') {
     return Boolean(
-      (ccdCase.noDecisionWithoutInspectionReason as string) &&
-      textAreaIsValidLength(ccdCase.noDecisionWithoutInspectionReason as string)
+      (ccdCase?.noDecisionWithoutInspectionReason as string) &&
+      textAreaIsValidLength(ccdCase?.noDecisionWithoutInspectionReason as string)
     );
   }
-  return ccdCase.agreeToDecisionWithoutInspection === 'yes';
+  return ccdCase?.agreeToDecisionWithoutInspection === 'yes';
 }
