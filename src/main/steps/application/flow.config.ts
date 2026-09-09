03b18d680d475850300de04c31ff2a7eaddfe323
Alex Buzea <60071473+AlexBuzea@users.noreply.github.com>
2026-09-09 16:50:33 +0100

    feat/HDPD-584-hardship-page (#350)
    * adding hardship page, updating heading for upload page and creating new placeholder pages for upload evidence of harship and cya page
    
    * adding unit test for clearing details when no is selected
    
    * fix multer cve issue

--- src/main/steps/application/flow.config.ts
+++ src/main/steps/application/flow.config.ts
@@ -27,6 +27,9 @@ export const flowConfig: JourneyFlowConfig = {
     'upload-evidence-notice-not-legally-valid': {
       showCondition: (req: Request) => getFormData(req, 'your-notice-proposing-a-new-rent').noticeLegallyValid === 'no',
     },
+    'upload-evidence-experience-hardship': {
+      showCondition: (req: Request) => getFormData(req, 'hardship').rentIncreaseCauseHardship === 'yes',
+    },
     'upload-evidence-improvements-or-repairs': {
       showCondition: (req: Request) => getFormData(req, 'repairs-and-improvements').hasRepairsAndImprovements === 'yes',
     },
