782905b933a812f01a55db3fbf80fd85facfef3e
Alex <alex.buzea@hmcts.net>
2026-09-21 11:24:13 +0100

    adding data mapping and prefilling the name if exists already

--- src/main/steps/application/tenant-details/your-information/index.ts
+++ src/main/steps/application/tenant-details/your-information/index.ts
@@ -1,6 +1,6 @@
 import { flowConfig } from '../../flow.config';
 
-import { createFormStep } from '@modules/steps';
+import { createFormStep, getFormDataString } from '@modules/steps';
 import type { StepDefinition } from '@modules/steps/stepFormData.interface';
 import { PTCaseData } from '@services/ccdCase.interface';
 
@@ -70,6 +70,29 @@ export const step: StepDefinition = createFormStep({
       classes: 'govuk-input--width-20',
     },
   ],
+  getInitialFormData: req => {
+    const caseData: PTCaseData | undefined = req.session.ccdCase;
+    const user = req.session.user;
+
+    const applicantFirstName =
+      getFormDataString(req, stepName, 'applicantFirstName') ?? caseData?.applicantFirstName ?? user?.givenName;
+
+    const applicantLastName =
+      getFormDataString(req, stepName, 'applicantLastName') ?? caseData?.applicantLastName ?? user?.familyName;
+
+    const companyName = getFormDataString(req, stepName, 'companyName') ?? caseData?.tenantDetails?.companyName;
+
+    const referenceNumberForCommunications =
+      getFormDataString(req, stepName, 'referenceNumberForCommunications') ??
+      caseData?.tenantDetails?.referenceNumberForCommunications;
+
+    return {
+      ...(applicantFirstName && { applicantFirstName }),
+      ...(applicantLastName && { applicantLastName }),
+      ...(companyName && { companyName }),
+      ...(referenceNumberForCommunications && { referenceNumberForCommunications }),
+    };
+  },
 });
 
 function isAnswered(ccdCase: PTCaseData | undefined): boolean {
