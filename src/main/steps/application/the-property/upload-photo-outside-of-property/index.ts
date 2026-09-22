fa3f97e51f9eb449db39feeb60f835f49a8a0359
Alex <alex.buzea@hmcts.net>
2026-09-22 12:53:51 +0100

    adding upload page for outside property photo

--- src/main/steps/application/the-property/upload-photo-outside-of-property/index.ts
+++ src/main/steps/application/the-property/upload-photo-outside-of-property/index.ts
@@ -1,10 +1,13 @@
 import { flowConfig } from '../../flow.config';
 
+import { readDocuments, toDisplayDocuments } from '@modules/documents/storage';
 import { createFormStep } from '@modules/steps';
 import type { StepDefinition } from '@modules/steps/stepFormData.interface';
+import { ACCEPT_ATTRIBUTE_EXTENSIONS } from '@utils/documentUploadValidation';
 
 const journeyName = 'application';
 const stepName = 'upload-photo-outside-of-property';
+const documentField = 'outsidePropertyDocument';
 
 export const step: StepDefinition = createFormStep({
   stepName,
@@ -13,10 +16,25 @@ export const step: StepDefinition = createFormStep({
   flowConfig,
   customTemplate: `${__dirname}/uploadPhotoOutsideOfProperty.njk`,
   showCancelButton: false,
-  isAnswered: () => false,
+  documentField,
+  isAnswered: () => true,
   translationKeys: {
     pageTitle: 'pageTitle',
     heading: 'heading',
+    subHeading: 'subheading',
   },
-  fields: [],
+  fields: [
+    {
+      name: 'documents',
+      type: 'file',
+      required: false,
+      accept: ACCEPT_ATTRIBUTE_EXTENSIONS,
+      isPageHeading: false,
+      labelClasses: 'govuk-body',
+      translationKey: { label: 'documentUpload.label' },
+    },
+  ],
+  getInitialFormData: async req => ({
+    documents: toDisplayDocuments(await readDocuments(req, documentField)),
+  }),
 });
